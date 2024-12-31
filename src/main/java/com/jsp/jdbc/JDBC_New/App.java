3d458afde250c2a595ce6377eef3b7f7f5673555
Deepak <deepakadhi0808@gmail.com>
2024-12-31 17:03:14 +0530

    Initial commit

---
+++ src/main/java/com/jsp/jdbc/JDBC_New/App.java
@@ -0,0 +1,13 @@
+package com.jsp.jdbc.JDBC_New;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
